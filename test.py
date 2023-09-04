41503250b9e9fcb2565f4751832cefc69ac9c320
蒋智 <962395743@qq.com>
2023-09-04 11:38:57 +0800

    [perf] 9月4日 上午：数据爬取部分循环的优化，以及部分错误的修改

---
+++ test.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def find(path):
+    global driver
+    return driver.find_element(by=By.XPATH, value=path).text
+
+url = 'https://www.lottery.gov.cn/jc/zqgdjj/?m=60000'
+# 构建驱动
+driver = webdriver.Chrome()
+# 前往网站
+driver.get('https://www.lottery.gov.cn/jc/zqgdjj/?m=60000')
+
+print(find('//*[@id="leagueMatch"]'))
